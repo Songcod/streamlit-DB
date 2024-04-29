377790a49208aaecd7119f66bb79428e42422488
Songcod <118869648+Songcod@users.noreply.github.com>
2024-04-29 11:23:08 +0900

    Add files via upload

--- 옷BTI_final.py
+++ 옷BTI_final.py
@@ -65,32 +65,32 @@ questions = [
     ("1. ‘오늘부터 단 7일! 고객님만을 위한 특별 할인 쿠폰!’ 카톡이 울렸다. 이때 나의선택은?", 
      ["옷을 살 생각은 없었지만 세일을 한다고 하니 일단 구경한다.", 
       "딱히 옷을 사려고 생각하지 않았으니 무시한다."], 
-      "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihY8Tmt5I7J642-aUUYWiqdqtAngOn7E8uurcvMG33Y984xM2-xuZqijWJS3ihPIM7z1GVZQP_IukRq44awsWdzc6BoU=w959-h910"),
+      "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZYzmHumCfTGLuR-tCuvVQtxEd7l9--q5XpqagKaRBm7IjORO_50pWuB1lWOz4I2QF6mFhXs1fJfb6q8kj7h9E3mcn8Ed9UoA=w958-h911-rw-v1"),
     ("2. 앗! 쿠폰이 오늘 사라져요! 고객님이 보유하신 쿠폰이 오늘 만료될 예정입니다.", 
      ["엇!! 내가 호옥시나 필요한 옷이 있을 수도 있으니 12시가 되기 전까지 시간될 때마다 웹서핑을 계속한다.", 
       "이런 타임 어택으로 옷을 사면 실패하거나 충동 구매할 확률이 높으니 다음 기회에!"], 
-      "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihYWI5bq0wMjDC84Z5qygX6pq0pkOC_XKyNG8tj_nSRsfHNhveQYCu1tWPXNE834r1jwTgRa7kvSfU3skPHfwWEJyiU3=w959-h910"),
+      "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihY5lVzt0tK9DaGLyIXjQhk5fjw_00PaKxFGx6fF52mhpX4NXCXBCwegtmIfNoAYnQsUPZt1Tbq7u80v02tiv-yHRZEdkFoRmw=w958-h911-rw-v1"),
     ("3. 새학기를 맞아 대청소를 결심하고 옷장 문을 열었더니", 
      ["오래 입지 않아 있는 줄도 몰랐던 옷들이 한 무더기로 발견된다.", 
       "매년 옷장 정리를 했기 때문에 어떤 옷들이 있는지 잘 알고 있다."], 
-      'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihaQA9xKJRmUQSG17SkJ2gkwVcdvzpBqWgkKVj3T-LR1q7gTIz5W7KYhn1DCOMnMCvqWW6y0DFfKQfmNHE9YMeiAYN1EKg=w959-h910'),
+      'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihY0LbKjzfY3GYd4Un7WNojq8wrKr7FznFij97Xhw8hiYaOET2cA5bjiQnnDC9EWmeImSpk07hnz-eo3WWseA-I-zlfDEOfmeGs=w958-h911-rw-v1'),
     ("4. 둘 중 하나를 구매해야 한다면?", 
      ["친환경 소재와 공정을 사용해 만든 코트", 
       "최신 트렌드를 반영해 만든 올해의 SS 상품"], 
-     "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihYD6vuDcZ4DuMOxzoIET1MA8yNEazydXUf6xCDyqlwd9glwBZ_vbWYYMsi7D1JtdJd3cbeK5jGlHRmBmGZ9ym9GQ0tKMQ=w959-h910"),
+     "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZZUMv-LhhNNBlELZbOalZHRH1-McrT9I904csiJTSJuTxBHc794fEXZn8A3i5wwCLHp78-x9v57ckmIBMzK0Ga4tnyxy3NIfM=w958-h911-rw-v1"),
     ("5. 오늘은 개강 첫 주! 아쉽게도 이번 학기는 공강이 없는데,,,", 
      ["적어도 일주일동안 겹치지 않고 입을 정도의 옷들은 있어야지!! 바로 쇼핑을 시작한다.", 
       "에이! 내 옷을 누가 알아본다고!! 적당히 돌려 입는다."], 
-     "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihYBEwJGAEaxY7gQHxh5Ie1JAsf5xMwJ01TCiytTKTKQepUFoq_ydQ2FmLel9lnr2wldyR-vwB0adooLJWhzDqOEiAr6Tg=w959-h910"),
+     "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZITuIE74RHxs8ENSaf9wwrR5dednZTcylRtbW08HmbrmxJVJpI4MOzM9Z27ALc02ioMuhc28FnpLcn9cK9sRE_5fl7h4GmTzg=w958-h911-rw-v1"),
     ("6. 옷장정리를 해서 안 입는 옷을 박스에 담았다. 이때 나의 선택은?", 
      ["에이… 언젠간 입겠지. 버리긴 아까우니 입을 일이 있을 때까지 놔둔다.", 
       "난 안 입지만 괜찮은 옷들인데… 헌옷 기부처를 알아본다.",
       "이번 기회에 리폼을 한 번 해볼까? 유튜브에 긴 청바지 리폼 방법을 찾아본다."], 
-     "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZB3z62F5vZn6GYlDzICIZ3CR-mJyl6wREpLOJDt69i95veOyiq0p80rA0CEvNm_KRJyqcaYzJerM3ZC4t42atTLG0aAg=w959-h910"),
+     "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihbyscGJQt5J1LYsAeiE-JIoytcabXJEgDX61TiItTVng4-4Yqn2QXLm3FNTWm3MsiTYBkFngi6bLDsJfbVOcPrPgnXrCzOOOZ8=w958-h911-rw-v1"),
      ("7. 우리집 앞에 옷 가게가 생긴다면 내가 더 애용할 곳은?", 
      ["상태가 좋은 구제 옷들을 이용해 다양한 코디를 선보이는 구제샵", 
       "값싼 소재의 옷을 이용해 가성비 좋게 옷을 구매할 수 있는 가게"], 
-     "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihbaEu3gUdtSwkRVuNEl5vNBDQnQkAt5OS9CJLcdcJA9efmieyJzDzyDUjtEVLdVV-BOO_vM8Rii_jamuS0_0EehaU7jWQ=w959-h910")
+     "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihbHrOWhsY8LLU465sosVtbKGWX6UvpKQYt3nw5uYcEbvlCy6sYqpxrbF0Q6fRlyMKlkyqJrcwgx202vzNfW08I0kizHO7eTn0U=w958-h911-rw-v1")
 ]
 
 # 앱의 초기 설정 부분에서 응답 DataFrame을 초기화합니다.
@@ -140,7 +140,7 @@ def display_cover():
     st.markdown(
         f"""
         <div style="display: flex; justify-content: center;">
-            <img src="https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihabHj4NDgk50cT1YvfjYO6fQ4WvYnGRqwWAJYyM4Ppf8ua-sYK_ChkH0ufxYzYZXEVewUT0DYUT9tmhtZ02ZU3dinQf=w959-h910" style="width: 384px; height: auto; max-height: 24rem;">
+            <img src="https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZiyzC1h1UW88uwfo5jRXJJzkQy3LJmcbkHXH-Tmmq_yTXVXfCzxKX9LTc-e3_iUxISBN_pxUffQYpOB6VX3Al3yqv1Iu9Z7w=w958-h911-rw-v1">
         </div>
         """,
         unsafe_allow_html=True,
@@ -157,10 +157,10 @@ def display_cover():
 
     # 캐릭터 순서와 이미지 URL을 업데이트
     images = [
-        "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihY8qdARdLOtkqoTRkqVq3jKVrgqRNnha0AlYUQVuBMBJ5jcDO6hhdBmVWf6cgI1wCp1P9GaRUfJwGh8C7P5silzW8KZ=w959-h910",  # 트렌드탐험대장
-        "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihapAmUipxeC3KwTNk33jRKo_HlwB0Fk2_lsqwrCQblBaIepCi2FeK703h1nU9yPib8-vZBRnJw5tQWCjxUOvcjFYlFRTQ=w959-h910",  # 모던스타일리스트
-        "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihYyCLJBOT80T0QdzwNNiKsPds_uzRUFIm5LBY1umuzATgkIUbVV5IsRGS3AUrFEmoUNmu6oS8jij-RcL0KHkd2luF3Z=w959-h910",  # 그린스타일리스트
-        "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihaWbM9u0o-6dTWCnk05EKdN4tyQ4l8CG-BIGzu96MUh_RtMApucw9aHRFwidUuHQ3BipqXAeT5vxfyy9AoUXqblYp1Nmg=w959-h910"   # 환경우주탐험가
+        "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihb-q1T6qUjbXCB1eDxdzLEf8-7k_HO_WM7HqJ9W96lzV7jPfBZTfO3UJPcdEZtjB9VVs_pU4rTcu8yLqYf-43pgAPYgTQVfPA=w958-h911-rw-v1",  # 트렌드탐험대장
+        "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZhE6TOVp7V3RRv8iV8IaF22FfUg1iRjbTBPZ3dHu18QFCBx5irWKu6g2EUZD1etHXFtCnaNxxoyH1hn_izjLxuRe43WCPL1CU=w958-h911-rw-v1",  # 모던스타일리스트
+        "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpiha_cdTc-GR1jJHKDk9eFF4I-MOHkLzpE7LLS0PfItMcdojvW-2cxDk6I9hZmZ008Y84xshh__TKWUHNSDBVtKN-fSxpRfZ4pg=w1920-h912-rw-v1",  # 그린스타일리스트
+        "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZyGlYA0q1oi3EcsP4W2a8EOY5orw45-MwvOvN4xlyZjzYtLQs9dXy0ypMimI17FE_IK7_OBj1Kht8HVo5yrsDDjCTI18rxFE0=w958-h911"   # 환경우주탐험가
     ]
 
     headers = ["트렌드탐험대장", "모던스타일리스트", "그린스타일리스트", "환경우주탐험가"]
@@ -204,7 +204,7 @@ def display_intro():
     st.markdown(
              f"""
              <div style="display: flex; justify-content: center;">
-                 <img src="https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihbKh5LIOXYet0VQbOcHJWBJ4gf3BLeliXBi_daSykPu7QWRmac2Ci4Hj7BNdpqCovnqCqNgTF3GwdVd9yVheUyWXIX0QQ=w959-h910" style="width: 384px">
+                 <img src="https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZ8ke3I319C4sT-aQnvY25Nxd7c-8B-pma12YWrLnk9ksZXiH5AZ9aV-ea6AxBqMedN4vM-5OXuHUbsKWg7LMf_otFnm9k1aIQ=w958-h911-rw-v1">
              </div>
              """,
              unsafe_allow_html=True,
@@ -251,27 +251,23 @@ def display_results():
     converted_scores = results.map(score_dict).tolist()
     total_score = sum(converted_scores)
 
-
     # 총점수에 따라 결과 이미지와 메시지, 그리고 이미지 설명을 결정합니다.
     if total_score >= 30:
-        image_url = "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihY8qdARdLOtkqoTRkqVq3jKVrgqRNnha0AlYUQVuBMBJ5jcDO6hhdBmVWf6cgI1wCp1P9GaRUfJwGh8C7P5silzW8KZ=w959-h910"
+        image_url = "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihb-q1T6qUjbXCB1eDxdzLEf8-7k_HO_WM7HqJ9W96lzV7jPfBZTfO3UJPcdEZtjB9VVs_pU4rTcu8yLqYf-43pgAPYgTQVfPA=w958-h911-rw-v1"
         message = "트렌드탐험대장"
-        description_url = 'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZCRyojk6XRJQHgdOIQo2HLPPJukenoQWSR44HKqfSe0tnKRbjrWfG6204Tqu2K_8hvfx1sEuNDXt8ORFVioPhdpxLwsg=w959-h910'
-
+        description_url = 'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihbYO9puQdrL4bqeb_a9VJ42hqa48a1k7NsAOA2vGMf5qFX5_8NyK2e8pP1en3t3UybTIxs8U9uE96IJWKxp6vE6V_LCVgK1uk0=w958-h911-rw-v1'
     elif total_score >= 20:
-        image_url = "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihapAmUipxeC3KwTNk33jRKo_HlwB0Fk2_lsqwrCQblBaIepCi2FeK703h1nU9yPib8-vZBRnJw5tQWCjxUOvcjFYlFRTQ=w959-h910"
+        image_url = "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZhE6TOVp7V3RRv8iV8IaF22FfUg1iRjbTBPZ3dHu18QFCBx5irWKu6g2EUZD1etHXFtCnaNxxoyH1hn_izjLxuRe43WCPL1CU=w958-h911-rw-v1"
         message = "모던스타일리스트"
-        description_url = 'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihbNasS1ivx-HVDEExWXbfcU7ppW8W4SMlxcGK3B9MldBNWni73ILvNZjLeVv9tzbNT5i31EnxN7PmAxg4g__-NojaSvTQ=w959-h910'
+        description_url = 'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZTpIBEchI5sBRq51G05WLrC8XC8U9RhkjVCc3wVFD-l1HToptkYFPQKB5FOtqtv64agmh_qY6zQE_ltxdxdgxY-aIePIEPilM=w958-h911-rw-v1'
     elif total_score >= 10:
-        image_url = "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihYyCLJBOT80T0QdzwNNiKsPds_uzRUFIm5LBY1umuzATgkIUbVV5IsRGS3AUrFEmoUNmu6oS8jij-RcL0KHkd2luF3Z=w959-h910"
+        image_url = "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpiha_cdTc-GR1jJHKDk9eFF4I-MOHkLzpE7LLS0PfItMcdojvW-2cxDk6I9hZmZ008Y84xshh__TKWUHNSDBVtKN-fSxpRfZ4pg=w1920-h912-rw-v1"
         message = "그린스타일리스트"
-        description_url = 'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihYFveJNzjD0nUzzXVEr1DgeqMkKoWN98frOtX9mswf1Wu4FYmFv_IGrbM3K7ghTbSMFflrxeweNW7a0oN8hdTIY785b=w959-h910'
-
-
+        description_url = 'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihaI_gbl8tBvXMwElSAJrFu03ynNVOIGYAxyQ3XThZ5ZGmhjQ_KKxEL7R_EGyNWP4PlGPu0KfmEzwPmhoDaxFAqjeViDzXaW4w=w958-h911-rw-v1'
     else:
-        image_url = "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihaWbM9u0o-6dTWCnk05EKdN4tyQ4l8CG-BIGzu96MUh_RtMApucw9aHRFwidUuHQ3BipqXAeT5vxfyy9AoUXqblYp1Nmg=w959-h910"
+        image_url = "https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihZyGlYA0q1oi3EcsP4W2a8EOY5orw45-MwvOvN4xlyZjzYtLQs9dXy0ypMimI17FE_IK7_OBj1Kht8HVo5yrsDDjCTI18rxFE0=w958-h911"
         message = "환경우주탐험가"
-        description_url = 'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihavbC5I6TcfL33V0_KIbOp6hMV0s5AhJOvHmhEHFpi3XMiB46Vk2KsdGuFKRet-OysBQ3drRPrUBHt4UD29nYLm_Hm2Qw=w959-h910'
+        description_url = 'https://lh3.googleusercontent.com/u/1/drive-viewer/AKGpihY_bjkDwpgR52lz_E01PaVc-sLKoitrQPbXcZLeRBtbzsWBc8-uAUjfNOVML3H4q-n_6dUHjIgyi3AMG7ANql3Pe_5bTVRsYbw=w958-h911-rw-v1'
 
     
     if not st.session_state.get('results_saved', False):
